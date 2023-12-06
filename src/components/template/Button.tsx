f57b578e623cac2e9ce34485b9b5a6614e9c8c85
Rikuto <orikutobasuke@gmail.com>
2023-12-06 18:21:35 +0900

    ButtonComponentの作成

---
+++ src/components/template/Button.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import style from '@/styles/components/template/button.module.scss'
+
+type Props = {
+    label: string,
+    className: string | undefined,
+    onClick: () => void,
+    link: string
+}
+
+export function Button(props: Props) {
+
+
+    const ButtonComponent = (
+        <button
+            onClick={props.onClick}
+            className={props.className ? props.className : style.btnStyle}
+        >
+            {props.label}
+        </button>
+    )
+
+    return (
+        <>
+            {props.link ? (
+                <a href={props.link}>
+                    {ButtonComponent}
+                </a>
+            ) : (
+                ButtonComponent
+            )}
+        </>
+    )
+}
