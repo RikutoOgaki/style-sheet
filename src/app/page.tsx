56f1b14040e7debe81bca2d3832afe5ed7859572
Rikuto <orikutobasuke@gmail.com>
2024-01-11 11:25:17 +0900

    topPage制作中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,24 @@
 import Image from 'next/image'
 import style from './page.module.scss'
 import { Button } from '@/components/template/Button'
+import { Header } from '@/components/template/Header'
 
 export default function Home() {
   return (
     <>
       <div className={style.wrap}>
+        <Header />
         <div className={style.firstView}>
-          <h1>StyleSheet<span className={style.accent}>-Pro</span></h1>
-          <div>
-            <Button label='Learn Started' />
-            <Button label='Git Hub' />
+          <h1 className={style.title}>StyleSheet<span className={style.accent}>-Pro</span></h1>
+          <div className={style.btnBox}>
+            <Button label='Learn Started →' className={style.learnStartBtn} />
+            <Button label='Git Hub' className={style.githubBtn} />
           </div>
           <div>
-            ↓
-            StyleSheet-Proって？
+            <p>
+              ↓
+              StyleSheet-Proって？
+            </p>
           </div>
         </div>
       </div>
--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import style from '@/styles/components/template/header.module.scss'
+import Link from 'next/link'
+import { FaGithub } from 'react-icons/fa'
 
 export function Header() {
     return (
         <>
             <header className={style.head}>
-
+                <p className={style.text}>v 1.0.0</p>
+                <Link href={'https://github.com/RikutoOgaki/style-sheet'}>
+                    <FaGithub style={{ color: '#262626', fontSize: '1.5rem' }} />
+                </Link>
             </header>
         </>
     )
