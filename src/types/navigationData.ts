ea549f3a2cd215df94a4808d7e4e1082377b67b4
Rikuto <orikutobasuke@gmail.com>
2024-01-11 10:28:44 +0900

    navigation制作中

--- src/types/navigationData.ts
+++ src/types/navigationData.ts
@@ -33,49 +33,5 @@ export const CssData = [
         cssName: 'Color',
         link: '/'
     },
-    {
-        id: 1,
-        cssName: 'Flex',
-        link: '/'
-    },
-    {
-        id: 2,
-        cssName: 'Grid',
-        link: '/'
-    },
-    {
-        id: 3,
-        cssName: 'Color',
-        link: '/'
-    },
-    {
-        id: 1,
-        cssName: 'Flex',
-        link: '/'
-    },
-    {
-        id: 2,
-        cssName: 'Grid',
-        link: '/'
-    },
-    {
-        id: 3,
-        cssName: 'Color',
-        link: '/'
-    },
-    {
-        id: 1,
-        cssName: 'Flex',
-        link: '/'
-    },
-    {
-        id: 2,
-        cssName: 'Grid',
-        link: '/'
-    },
-    {
-        id: 3,
-        cssName: 'Color',
-        link: '/'
-    },
+
 ]
